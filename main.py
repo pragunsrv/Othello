49505dfd052e290b3d40a73dc9cd7334f5326490
Pragun <pragun1106@gmail.com>
2024-08-12 10:39:26 +0530

    New Features
    Score Calculation: Tracks and displays the score for each player.
    Move Suggestions: Highlights possible moves for the current player.
    Game Over Condition: Determines the winner based on the score when no more moves are available.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class Othello:
     def __init__(self):
         self.board = self.create_board()
         self.current_player = 'B'  # B for Black, W for White
+        self.score = {'B': 2, 'W': 2}  # Initial score with 2 pieces each
 
     def create_board(self):
         board = [[' ' for _ in range(8)] for _ in range(8)]
@@ -12,8 +13,10 @@ class Othello:
         return board
 
     def print_board(self):
-        for row in self.board:
-            print(' '.join(row))
+        print("  " + " ".join(map(str, range(8))))
+        for i, row in enumerate(self.board):
+            print(str(i) + " " + ' '.join(row))
+        print(f"Score -> Black: {self.score['B']}, White: {self.score['W']}")
         print()
 
     def is_valid_move(self, row, col, player):
@@ -38,6 +41,7 @@ class Othello:
             return False
         self.board[row][col] = self.current_player
         self.flip_discs(row, col)
+        self.update_score()
         self.current_player = 'W' if self.current_player == 'B' else 'B'
         return True
 
@@ -55,6 +59,13 @@ class Othello:
                 for rr, cc in discs_to_flip:
                     self.board[rr][cc] = self.current_player
 
+    def update_score(self):
+        self.score = {'B': 0, 'W': 0}
+        for row in self.board:
+            for cell in row:
+                if cell in self.score:
+                    self.score[cell] += 1
+
     def has_valid_moves(self):
         for row in range(8):
             for col in range(8):
@@ -62,15 +73,39 @@ class Othello:
                     return True
         return False
 
+    def get_valid_moves(self):
+        valid_moves = []
+        for row in range(8):
+            for col in range(8):
+                if self.is_valid_move(row, col, self.current_player):
+                    valid_moves.append((row, col))
+        return valid_moves
+
+    def suggest_moves(self):
+        valid_moves = self.get_valid_moves()
+        for row, col in valid_moves:
+            print(f"Suggested move: ({row}, {col})")
+        print()
+
     def play_game(self):
         self.print_board()
         while self.has_valid_moves():
+            self.suggest_moves()
             row, col = map(int, input(f"Player {self.current_player}, enter your move (row col): ").split())
             if self.make_move(row, col):
                 self.print_board()
             else:
                 print("Invalid move. Try again.")
-        print("Game over!")
+        self.declare_winner()
+
+    def declare_winner(self):
+        if self.score['B'] > self.score['W']:
+            print("Black wins!")
+        elif self.score['W'] > self.score['B']:
+            print("White wins!")
+        else:
+            print("It's a tie!")
+        print("Final Score -> Black: {}, White: {}".format(self.score['B'], self.score['W']))
 
 
 if __name__ == "__main__":
